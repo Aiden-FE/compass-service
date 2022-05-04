2a14261e1802fce87c85f09dc66b47c5f8566719
Aiden <31586885+Aiden-FE@users.noreply.github.com>
2022-05-04 18:18:27 +0800

    upgrade config

--- debug.js
+++ debug.js
@@ -1,5 +1,9 @@
-console.log('debug env: ',
-    process.env.DATABASE_URL,
-    process.env.ALI_CLOUD_SMS_ACCESS_KEY_ID,
-    process.env
-)
+const shell = require('shelljs')
+// console.log('debug env: ',
+//     process.env.DATABASE_URL,
+//     process.env.ALI_CLOUD_SMS_ACCESS_KEY_ID,
+//     process.env
+// )
+
+const catData = shell.cat('.env').toString('uat8')
+console.log('Debug: ', catData, shell.ls('-a').toString('uat8'))
