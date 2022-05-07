77baa4a6d0c59edf1a76841050a16c569e27790b
Aiden <31586885+Aiden-FE@users.noreply.github.com>
2022-05-07 21:43:16 +0800

    feat(categories): Init module

--- apps/compass/src/modules/index.ts
+++ apps/compass/src/modules/index.ts
@@ -2,5 +2,6 @@ import { UsersModule } from './users/users.module';
 import { PermissionsModule } from './permissions/permissions.module';
 import { RolesModule } from './roles/roles.module';
 import { OpenModule } from './open/open.module';
+import {CategoriesModule} from "./categories/categories.module";
 
-export default [UsersModule, PermissionsModule, RolesModule, OpenModule];
+export default [UsersModule, PermissionsModule, RolesModule, OpenModule, CategoriesModule];
