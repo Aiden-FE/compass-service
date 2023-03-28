7fe709d88bc0b97f8a0db5a967fd229b0f6741e5
Aiden-FE <Aiden_FE@outlook.com>
2023-03-28 13:38:12 +0800

    feat(#13): support swagger

---
+++ shared/plugins/swagger.plugin.ts
@@ -0,0 +1,25 @@
+import { INestApplication, Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { version } from '../../package.json';
+
+export default function provideSwaggerPlugin(
+  app: INestApplication,
+  option: {
+    title: string;
+    version?: string;
+    description?: string;
+    path?: string;
+  },
+) {
+  const docsPath = option.path || `/docs/${(option.version || version).replace(/\./g, '-')}`;
+  const apiDocOptions = new DocumentBuilder()
+    .setTitle(option.title)
+    .setDescription(option.description)
+    .setVersion(option.version || version)
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, apiDocOptions);
+  SwaggerModule.setup(docsPath, app, document);
+  Logger.log(`The access path for the swagger document is "${docsPath}"`, 'SwaggerDocument');
+}
