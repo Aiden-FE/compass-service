74dd1f6d0be2ec904df85ffa0808de52dde6cff2
Aiden-FE <Aiden_FE@outlook.com>
2023-03-29 20:31:23 +0800

    feat(#13): support prisma orm

--- apps/compass-service/src/plugins/index.ts
+++ apps/compass-service/src/plugins/index.ts
@@ -1,5 +1,6 @@
 import { INestApplication, ValidationPipe, VersioningType } from '@nestjs/common';
 import { provideSwaggerPlugin, ResponseInterceptor } from '@shared';
+import { DBService } from '@app/db';
 
 export default async function providePlugins(app: INestApplication) {
   // 接口多版本
@@ -25,4 +26,7 @@ export default async function providePlugins(app: INestApplication) {
     }),
   );
   app.useGlobalInterceptors(new ResponseInterceptor());
+  // 断开连接前需要关闭db连接
+  const dbService = app.get(DBService);
+  await dbService.enableShutdownHooks(app);
 }
