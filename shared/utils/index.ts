96cdcc1da74e9f6007ba2478ea1b5ed00a53f5e1
Aiden-FE <Aiden_FE@outlook.com>
2023-03-29 17:50:05 +0800

    wip: save

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,1 +1,22 @@
+import { DEFAULT_PAGE_NUM, DEFAULT_PAGE_SIZE } from '@shared/config';
+import { PaginationRequestFromURLDto } from '@shared/interfaces';
+
 export * from './env';
+
+/**
+ * @description 包装分页查询参数,确保查询参数正确
+ * @param pagination
+ */
+export function generatePaginationSQLParams(pagination: PaginationRequestFromURLDto) {
+  const result = {
+    skip: DEFAULT_PAGE_NUM,
+    take: DEFAULT_PAGE_SIZE,
+  };
+  if (pagination.pageSize) {
+    result.take = Number(pagination.pageSize);
+  }
+  if (pagination.pageNum !== undefined) {
+    result.skip = Number(pagination.pageNum) * result.take;
+  }
+  return result;
+}
