e50ff3b50a5b7b8e3d2393c0898ec051d8bf0b1d
Aiden-FE <aiden_fe@outlook.com>
2023-03-31 21:57:05 +0800

    feat(#13): 支持日志中间件

--- shared/index.ts
+++ shared/index.ts
@@ -4,3 +4,4 @@ export * from './config';
 export * from './interfaces';
 export * from './interceptors';
 export * from './decorators';
+export * from './middleware';
---
+++ shared/middleware/index.ts
@@ -0,0 +1,1 @@
+export { default as LoggerMiddleware } from './logger.middleware';
